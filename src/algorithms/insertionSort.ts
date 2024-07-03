a8fd686d925f9d7f606bdcf92a47db0e1c1d7dfb
Binitstha <binitcr336@gmail.com>
2024-07-03 23:10:28 +0545

    final commit

--- src/algorithms/insertionSort.ts
+++ src/algorithms/insertionSort.ts
@@ -6,28 +6,22 @@ const insertionSort = (array: number[], animations: Animations) => {
   for (let i = 1; i < n; i++) {
     let key = array[i];
     let j = i - 1;
-
-    // Record animation for key comparison
+    
     animations.push([[i, j], false]);
 
     while (j >= 0 && array[j] > key) {
-      // Record animation for swapping elements
       animations.push([[j + 1, array[j]], true]);
-      // Shift element to the right
       array[j + 1] = array[j];
       j--;
 
       if (j >= 0) {
-        // Record animation for key comparison
         animations.push([[i, j], false]);
       }
     }
 
-    // Place key in correct position
     array[j + 1] = key;
 
     if (j + 1 !== i) {
-      // Record animation for placing key in correct position
       animations.push([[j + 1, key], true]);
     }
   }
